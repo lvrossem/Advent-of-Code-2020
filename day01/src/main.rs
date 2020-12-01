0d20f0f719f4904fdbcf5d064c61db3bf9376b8c
lvrossem <lukas.vanrossem1@gmail.com>
2020-12-01 19:15:03 +0100

    Bit of cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,53 +5,62 @@ fn main() -> io::Result<()> {
     let file = File::open("day01_input.txt")?;
     let reader = BufReader::new(file);
 
-    let mut numbers: Vec<i32> = Vec::new();
+    let mut numbers: Vec<u32> = Vec::new();
 
     for line in reader.lines() {
-        let parsed_int = line.unwrap().parse::<i32>().unwrap();
+        let parsed_int = line.unwrap().parse::<u32>().unwrap();
         numbers.push(parsed_int);
     }
 
+    let length = numbers.len();
+
     numbers.sort();
 
-    let mut start = 0;
-    let mut end = numbers.len() - 1;
+    let mut start:usize = 0;
+    let mut end:usize = length - 1;
 
-    while numbers[start] + numbers[end] != 2020 {
-        if numbers[start] + numbers[end] > 2020 {
+    let mut sum:u32 = numbers[start] + numbers[end];
+
+    while sum != 2020 {
+        if sum > 2020 {
             end -= 1;
         } else {
             start += 1;
         }
+
+        sum = numbers[start] + numbers[end];
     }
 
     println!("The result for 2 numbers is {}", numbers[start] * numbers[end]);
 
     start = 0;
-    end = numbers.len() - 1;
+    end = length - 1;
 
-    let mut found = false;
-    let mut third = 0;
+    let mut found:bool = false;
+    let mut third:usize = 0;
 
-    while !found && third < numbers.len() {
-        while numbers[start] + numbers[end] + numbers[third] != 2020 && end >= start {
-            if numbers[start] + numbers[end] + numbers[third] > 2020 {
+    sum = numbers[start] + numbers[end] + numbers[third];
+
+    while !found && third < length {
+        while sum != 2020 && end >= start {
+            if sum > 2020 {
                 end -= 1;
             } else {
                 start += 1;
             }
+
+            sum = numbers[start] + numbers[end] + numbers[third];
         }
 
-        if numbers[start] + numbers[end] + numbers[third] == 2020 {
+        if sum == 2020 {
             found = true;
             println!("The result for 3 numbers is {}", numbers[start] * numbers[end] * numbers[third])
         } else {
             third += 1;
             start = 0;
-            end = numbers.len() - 1;
+            end = length - 1;
         }
     }
-    
 
     Ok(())
 }
