f6a3ac2e5d13883c74474a7f5cf4f2d484911dc6
lvrossem <lukas.vanrossem1@gmail.com>
2020-12-02 20:22:58 +0100

    Clean day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,25 +1,19 @@
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+fn main() {
+    let mut numbers: Vec<usize> = Vec::new();
 
-fn main() -> io::Result<()> {
-    let file = File::open("day01_input.txt")?;
-    let reader = BufReader::new(file);
-
-    let mut numbers: Vec<u32> = Vec::new();
-
-    for line in reader.lines() {
-        let parsed_int = line.unwrap().parse::<u32>().unwrap();
-        numbers.push(parsed_int);
-    }
+    std::fs::read_to_string("day01_input.txt")
+        .expect("file not found!")
+        .lines()
+        .for_each(|x| numbers.push(x.parse::<usize>().unwrap()));
 
     let length = numbers.len();
 
     numbers.sort();
 
-    let mut start:usize = 0;
-    let mut end:usize = length - 1;
+    let mut start = 0;
+    let mut end = length - 1;
 
-    let mut sum:u32 = numbers[start] + numbers[end];
+    let mut sum = numbers[start] + numbers[end];
 
     while sum != 2020 {
         if sum > 2020 {
@@ -36,8 +30,8 @@ fn main() -> io::Result<()> {
     start = 0;
     end = length - 1;
 
-    let mut found:bool = false;
-    let mut third:usize = 0;
+    let mut found = false;
+    let mut third = 0;
 
     sum = numbers[start] + numbers[end] + numbers[third];
 
@@ -61,6 +55,4 @@ fn main() -> io::Result<()> {
             end = length - 1;
         }
     }
-
-    Ok(())
 }
