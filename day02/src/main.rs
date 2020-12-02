572c805feb0fef075a4d97e68113c7206e7ca86c
lvrossem <lukas.vanrossem1@gmail.com>
2020-12-02 15:44:46 +0100

    Use Line-struct instead of 4-tuple

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,10 @@
+struct Line<'a> {
+    min: usize,
+    max: usize,
+    character: u8,
+    password: &'a str
+}
+
 fn main() {
     let mut amount = std::fs::read_to_string("day02_input.txt")
         .expect("file not found!")
@@ -18,27 +25,27 @@ fn main() {
     println!("There are {} valid passwords for part 2", amount);
 }
 
-fn parse(line: &str) -> (usize, usize, u8, &str) {
+fn parse(line: &str) -> Line {
     let split_line = line.split(' ').collect::<Vec<&str>>();
     let amount = split_line[0].split('-').collect::<Vec<&str>>();
 
-    let min = amount[0].parse::<usize>().unwrap();
-    let max = amount[1].parse::<usize>().unwrap();
-
-    let character = split_line[1].as_bytes()[0];
-    
-    return (min, max, character, split_line[2]);
+    return Line {
+        min: amount[0].parse::<usize>().unwrap(),
+        max: amount[1].parse::<usize>().unwrap(),
+        character: split_line[1].as_bytes()[0],
+        password: split_line[2]
+    }
 }
 
 fn check_part1(line: &str) -> bool {
-    let (min, max, character, password) = parse(line);
-    let occurences = password.matches(character as char).count();
+    let parsed = parse(line);
+    let occurences = parsed.password.matches(parsed.character as char).count();
 
-    return occurences >= min && occurences <= max;
+    return occurences >= parsed.min && occurences <= parsed.max;
 }
 
 fn check_part2(line: &str) -> bool {
-    let (min, max, character, password) = parse(line);
+    let parsed = parse(line);
     
-    return (password.as_bytes()[min - 1] == character) != (password.as_bytes()[max - 1] == character);
+    return (parsed.password.as_bytes()[parsed.min - 1] == parsed.character) != (parsed.password.as_bytes()[parsed.max - 1] == parsed.character);
 }
