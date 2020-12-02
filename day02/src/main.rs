e4b17065c52d268e1765c0a2b3cd46c737ec0a7a
lvrossem <lukas.vanrossem1@gmail.com>
2020-12-02 18:55:57 +0100

    Use impl for Line in day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,15 +1,38 @@
-struct Line<'a> {
+struct Line {
     min: usize,
     max: usize,
     character: u8,
-    password: &'a str
+    password: String
+}
+
+impl Line {
+    fn parse(line: &str) -> Line {
+        let split_line = line.split(' ').collect::<Vec<&str>>();
+        let amount = split_line[0].split('-').collect::<Vec<&str>>();
+
+        return Line {
+            min: amount[0].parse::<usize>().unwrap(),
+            max: amount[1].parse::<usize>().unwrap(),
+            character: split_line[1].as_bytes()[0],
+            password: split_line[2].to_string()
+        }
+    }
+
+    fn check1(&self) -> bool {
+        let occurences = self.password.matches(self.character as char).count();
+        return occurences >= self.min && occurences <= self.max;
+    }
+
+    fn check2(&self) -> bool {
+        return (self.password.as_bytes()[self.min - 1] == self.character) != (self.password.as_bytes()[self.max - 1] == self.character);
+    }
 }
 
 fn main() {
     let mut amount = std::fs::read_to_string("day02_input.txt")
         .expect("file not found!")
         .lines()
-        .map(check_part1)
+        .map(|x| Line::parse(x).check1())
         .filter(|x| *x)
         .count();
 
@@ -18,34 +41,9 @@ fn main() {
     amount = std::fs::read_to_string("day02_input.txt")
         .expect("file not found!")
         .lines()
-        .map(check_part2)
+        .map(|x| Line::parse(x).check2())
         .filter(|x| *x)
         .count();
 
     println!("There are {} valid passwords for part 2", amount);
-}
-
-fn parse(line: &str) -> Line {
-    let split_line = line.split(' ').collect::<Vec<&str>>();
-    let amount = split_line[0].split('-').collect::<Vec<&str>>();
-
-    return Line {
-        min: amount[0].parse::<usize>().unwrap(),
-        max: amount[1].parse::<usize>().unwrap(),
-        character: split_line[1].as_bytes()[0],
-        password: split_line[2]
-    }
-}
-
-fn check_part1(line: &str) -> bool {
-    let parsed = parse(line);
-    let occurences = parsed.password.matches(parsed.character as char).count();
-
-    return occurences >= parsed.min && occurences <= parsed.max;
-}
-
-fn check_part2(line: &str) -> bool {
-    let parsed = parse(line);
-    
-    return (parsed.password.as_bytes()[parsed.min - 1] == parsed.character) != (parsed.password.as_bytes()[parsed.max - 1] == parsed.character);
 }
