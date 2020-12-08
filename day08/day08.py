6ebc3744c3f8ffcfbc28626f329beb86d6a94cd1
lvrossem <lukas.vanrossem1@gmail.com>
2020-12-08 14:50:20 +0100

    Clean day 8

--- day08/day08.py
+++ day08/day08.py
@@ -1,12 +1,18 @@
-def part1():
-    input = open("day08_input.txt", 'r')
+def replace(instruction):
+    if 'nop' in instruction:
+        return 'jmp' + instruction[3:]
+    elif 'jmp' in instruction:
+        return 'nop' + instruction[3:]
 
-    lines = input.readlines()
-
+def run_program(lines, replaced=0, should_replace=False):
     accumulator, current, looped, visited = 0, 0, False, []
 
-    while not looped:
+    while not looped and current != len(lines):
         instruction = lines[current]
+
+        if should_replace:
+            if current == replaced and ('nop' in instruction or 'jmp' in instruction):
+                instruction = replace(instruction)
 
         if current in visited:
             looped = True
@@ -15,78 +21,39 @@ def part1():
             if 'nop' in instruction:
                 current += 1
             elif 'acc' in instruction:
-                mul = 1
-                if instruction[4] == '-':
-                    mul = -1
-                
-                number = int(instruction[5:])
-
-                accumulator += number * mul
+                accumulator += int(instruction[4:])
                 current += 1
             elif 'jmp' in instruction:
-                mul = 1
-                if '-' in instruction:
-                    mul = -1
-                
-                number = int(instruction[5:])
+                current += int(instruction[4:])
 
-                current += number * mul
+    return accumulator, current == len(lines)
+
+def get_input():
+    input = open("day08_input.txt", 'r')
+    lines = input.readlines()
+    input.close()
+    return lines
+
+def part1():
+    lines = get_input()
+
+    accumulator, complete = run_program(lines)
 
     print("Part 1: {}".format(accumulator))
 
-    input.close()
-
-def replace(instruction):
-    if 'nop' in instruction:
-        return 'jmp' + instruction[3:]
-    elif 'jmp' in instruction:
-        return 'nop' + instruction[3:]
-
 def part2():
-    input = open("day08_input.txt", 'r')
+    lines, replaced = get_input(), 0
 
-    lines = input.readlines()
+    length = len(lines)
 
-    replaced = 0
-
-    while replaced < len(lines):
-        accumulator, current, looped, visited = 0, 0, False, []
-
-        while not looped and current != len(lines):
-            instruction = lines[current]
-
-            if current == replaced and ('nop' in instruction or 'jmp' in instruction):
-                instruction = replace(instruction)
-
-            if current in visited:
-                looped = True
-            else:
-                visited.append(current)
-                if 'nop' in instruction:
-                    current += 1
-                elif 'acc' in instruction:
-                    mul = 1
-                    if instruction[4] == '-':
-                        mul = -1
-                    
-                    number = int(instruction[5:])
-
-                    accumulator += number * mul
-                    current += 1
-                elif 'jmp' in instruction:
-                    mul = 1
-                    if '-' in instruction:
-                        mul = -1
-                    
-                    number = int(instruction[5:])
-
-                    current += number * mul
+    while replaced < length:
+        accumulator, complete = run_program(lines, replaced, True)
+        
         replaced += 1
 
-        if current == len(lines):
-            print("Part 2: " + str(accumulator))
-            replaced = len(lines)
+        if complete:
+            print("Part 2: {}".format(accumulator))
+            replaced = length
 
 part1()
 part2()
-
